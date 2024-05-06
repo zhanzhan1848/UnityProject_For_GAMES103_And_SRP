9f9c1cca701787a2e1c4349cbb7ef37b51fe69a7
zhanzhan1848 <270428890@qq.com>
2024-05-06 19:11:17 +0800

    little update

--- Assets/ZZRenderer/RendererPipeline/ZZRendererPipelineAsset.cs
+++ Assets/ZZRenderer/RendererPipeline/ZZRendererPipelineAsset.cs
@@ -50,7 +50,7 @@ public class ZZRenderPipeline : RenderPipeline
         foreach(var camera in cameras)
         {
             RenderOpaqueObjectPerCamera(context, camera);
-            // RenderTransparentObjectPerCamera(context, camera);
+            RenderTransparentObjectPerCamera(context, camera);
         }
 
         context.Submit();
@@ -108,11 +108,28 @@ public class ZZRenderPipeline : RenderPipeline
         context.DrawSkybox(camera);
         // 裁剪场景
         camera.TryGetCullingParameters(out var cullingParams);
+        // 设置shadowDistance，不让shadowmap的距离过大导致根本看不到物体
+        cullingParams.shadowDistance = 10;
         var cullingResults = context.Cull(ref cullingParams);
+        // 新增自定义灯光类后，必须添加以支持新灯光
+        var lightData = _lightConfigurator.SetupShaderLightParams(context, ref cullingResults);
+
+        // 投影pass
+        _shadowCastPass.Execute(context, camera, ref cullingResults, ref lightData);
+
+        // 重设camera 相关参数
+        context.SetupCameraProperties(camera);
+
+        // 清除相机背景
+        // ClearCameraTarget(context, camera);
 
         // 计算物体渲染时的排序
         var sortingSetting = new SortingSettings(camera);
-        var drawSetting = new DrawingSettings(_shaderTag, sortingSetting);
+        var drawSetting = new DrawingSettings();
+        drawSetting.SetShaderPassName(1, _shaderTag);
+        drawSetting.SetShaderPassName(2, _shaderTag_ZForward);
+        drawSetting.sortingSettings = sortingSetting;
+        // drawSetting.enableDynamicBatching = true;
         // 过滤需要渲染的物体
         var filterSetting = new FilteringSettings(RenderQueueRange.transparent);
         // 绘制物体
